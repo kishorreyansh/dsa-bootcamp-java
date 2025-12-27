61839d9553e25a95d656a28e787da81f094c2830
kishorreyansh <kishor.andekar2389@gmail.com>
2025-12-27 13:22:11 -0500

    strings example

---
+++ 12-strings/src/PrettyPrinting.java
@@ -0,0 +1,14 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float num = 3.194372490f;
+        System.out.printf("Formatted Value %.2f", num);
+
+        System.out.println();
+
+        System.out.printf("Pie Value %.3f", Math.PI);
+
+        System.out.println();
+        System.out.printf("Hello, My name is %s and I am a %s","Kohli","Crickter");
+
+    }
+}
